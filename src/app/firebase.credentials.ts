0f1eac29b4d57ddbf4d5e578dd2d2d2bd3388792
Jose Witjaksono <hpsx80@gmail.com>
2019-01-10 16:07:39 +0700

    Firebase Credential

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAAkT5FxqKz2xiro_IefUAG_f-08anyHz4",
+    authDomain: "ionicshoppinglist-6112e.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglist-6112e.firebaseio.com",
+    projectId: "ionicshoppinglist-6112e",
+    storageBucket: "ionicshoppinglist-6112e.appspot.com",
+    messagingSenderId: "839737622882"
+}
